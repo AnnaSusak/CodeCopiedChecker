0560e4a551abd0192928149b659edc2a11976f7a
Аня Сусак <anna.i.susak@gmail.com>
2024-01-30 12:38:28 +0300

    fix bug with deleting extra tokens

--- main.py
+++ main.py
@@ -2,11 +2,7 @@ import re
 from Levenshtein import distance as lev
 
 
-def format_code(code):
-    # code = code.replace('\n', '').replace('\r', '').replace('\t', '').replace(' ','')
-    code = re.sub(r'\{[^\}]+\}', '', code)  # delete pascal comments
-    # code = ' '.join(code.split())
-    return code
+
 
 
 def get_tokens(code):
@@ -28,9 +24,26 @@ def get_tokens(code):
                'ln', 'exp', 'sqr', 'sqrt', 'pi', 'round', 'trunc', 'frac', 'random', 'odd', 'ord',
                'chr', 'pred', 'succ', 'readln', 'write', 'Assign', 'Reset', 'ReWrite', 'Append',
                'Close', 'Read', 'ReadLn', 'Write', 'WriteLn', ',', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0'}
-    for i in range(len(code)):
-        print(cur_w)
-        if code[i] == ' ' or (code[i] in special and code[i] not in '0123456789'):
+    i = 0
+    while i < len(code):
+        #print(cur_w)
+        if code[i] == '{':
+            cur = '{'
+            while i < len(code) and code[i] != '}':
+                i+=1
+                cur += code[i]
+            i+=1
+            print ('comment', cur)
+            tokens.append(['comment', 1])
+        elif code[i] == "'":
+            cur = "'"
+            while i < len(code) and code[i] != "'":
+                i+=1
+                cur += code[i]
+            i+=1
+            print ('string value', cur)
+            tokens.append(['string value', 1])
+        elif code[i] == ' ' or (code[i] in special and code[i] not in '0123456789'):
             elem = code[i]
             if cur_w != '':
                 if cur_w in special:
@@ -40,11 +53,12 @@ def get_tokens(code):
                     print('variable', cur_w)
                     tokens.append(['variable', 1])
                 cur_w = ''
-            counter = 0
-            while (i < len(code) and (code[i] == elem or (code[i] in '0123456789' and elem in '0123456789'))):
+            counter = 1
+            while (i < len(code) - 1 and (code[i + 1] == elem or (code[i + 1] in '0123456789' and elem in '0123456789'))):
                 counter += 1
                 i+=1
             tokens.append([elem, counter])
+            print('elem', elem, tokens[-1])
             cur_w = ''
         elif code[i] in special and code[i] in '0123456789':
             cur = code[i]
@@ -59,31 +73,62 @@ def get_tokens(code):
                 i+=1
             tokens.append([cur_w, 1])
             cur_w = '''
+        elif cur_w in special:
+            tokens.append([cur_w, 1])
         else:
             cur_w += code[i]
-    i = 1
-    while i < len(tokens):
-        if tokens[i][0] == tokens[i - 1][0]:
-            tokens.remove(tokens[i])
-            i-=1
-        elif tokens[i][0][0] in '0123456789' and tokens[i- 1][0][0] in '0123456789':
-            tokens.remove(tokens[i])
-            i -= 1
-        elif tokens[i][0] == '=' and tokens[i-1][0] ==':':
-            tokens[i - 1][0] = ':='
-            tokens.remove(tokens[i])
-            i -= 1
         i+=1
+    k = 1
+    print(tokens)
+    while k < len(tokens):
+        if tokens[k][0] == tokens[k - 1][0]:
+            print('remove', k, tokens[k])
+            tokens.pop(k)
+            k-=1
+           # print(tokens)
+        elif tokens[k][0][0] in '0123456789' and tokens[k- 1][0][0] in '0123456789':
+            print('remove', k, tokens[k])
+            tokens.pop(k)
+            k -= 1
+        elif tokens[k][0] == '=' and tokens[k-1][0] ==':':
+            tokens[k - 1][0] = ':='
+            print('remove', k, tokens[k])
+            tokens.pop(k)
+            k -= 1
+        elif tokens[k][0] == '}':
+            print('remove', k, tokens[k])
+            tokens.pop(k)
+            k-=1
+        k+=1
     return tokens
 
 
-'''with open('test_code1.txt', 'r', encoding="utf-8") as file:
-    code1 = file.read()'''
+with open('test_code1.txt', 'r', encoding="utf-8") as file:
+    code1 = file.read()
 # code1 = format_code((code1))
 with open('test_code2.txt', 'r', encoding="utf-8") as file:
     code2 = file.read()
-code2 = format_code(code2)
+#code2 = format_code(code2)
 # print(code1)
-print(get_tokens(code2))
+t1=get_tokens(code1)
+t2=get_tokens(code2)
+print(t1)
+print(t2)
+i = 0
+j = 0
+flag = False
+while i < len(t1) and j < len(t2):
+    while i < len(t1) and t1[i][0] == 'comment' or t1[i][0] == ' ' or t1[i][0] == 'string value':
+        i+=1
+    while j < len(t2) and t2[j][0] == 'comment' or t2[j][0] == ' ' or t2[i][0] == 'string value':
+        j+=1
+    if t1[i][0] != t2[j][0]:
+        print(i, j, t1[i][0], t2[j][0])
+        flag = True
+        break
+    i+=1
+    j+=1
+if not flag:
+    print('100 %')
 '''if lev(code1, code2) == 0:
     print('100%')'''
