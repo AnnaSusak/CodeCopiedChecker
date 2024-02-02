6144b0d09ce7e4fcce35ee00e622fc161a82c589
Аня Сусак <anna.i.susak@gmail.com>
2024-02-02 14:20:07 +0300

    add tokenize_code_numbers file

--- tokenize_code_numbers.py
+++ tokenize_code_numbers.py
@@ -1,3 +1,28 @@
+COMMENT_NUM = 143
+STRING_VALUE_NUM = 144
+VARIABLE_NUM = 145
 code_numbers = {
-
+    '+': 1, '-': 2, '*': 3, '/': 4, ':': 5, '=': 6,
+    '<': 7, '>': 8, '.': 9, '(': 10, ')': 11, '[': 12,
+    ']': 13, '{': 14, '}': 15, ':': 16, ';': 17, '\'': 18,
+    "\"": 19, '$': 20, '@': 21, '#': 22, '&': 23, '^': 24,
+    '_': 25, '~': 26, '%': 27, 'and': 28, 'end': 29, 'nil': 30,
+    'set': 31, 'array': 32, 'file': 37, 'not': 38, 'then': 39,
+    'begin': 40, 'for': 41, 'of': 42, 'to': 43, 'case': 44,
+    'function': 45, 'or': 47, 'type': 48, 'const': 49, 'goto': 50,
+    'packed': 51, 'until': 52, 'div': 53, 'if': 54, 'procedure': 55,
+    'var': 56, 'do': 57, 'in': 58, 'program': 59, 'while': 60, 'downto': 61,
+    'label': 62, 'record': 63, 'with': 64, 'else': 65, 'mod': 66, 'repeat': 67,
+    'writeln': 68, 'maxlongint': 69, ';': 70, 'maxint': 71, 'read': 72, 'integer': 73,
+    'real': 74, 'boolean': 75, 'char': 76, 'string': 77, 'Byte': 78, 'ShortInt': 79,
+    'Word': 80, 'Comp': 81, 'Integer': 82, 'LongInt': 83, 'Real': 84, 'Single': 85,
+    'Double': 87, 'Extended': 88, 'Boolean': 89, 'Char': 90, 'Array': 91, 'String': 92,
+    'Record': 93, 'Set': 94, 'Text': 95, 'File': 96, '>=': 97, '<=': 98, 'or': 99,
+    'not': 100, 'xor': 101, 'true': 102, 'false': 103, 'sin': 104, 'cos': 105, 'arctan': 106,
+    'abs': 107, 'ln': 108, 'exp': 109, 'sqr': 110, 'sqrt': 111, 'pi': 112, 'round': 113,
+    'trunc': 114, 'frac': 115, 'random': 116, 'odd': 117, 'ord': 118, 'chr': 119, 'pred': 120,
+    'succ': 121, 'readln': 122, 'write': 123, 'Assign': 124, 'Reset': 125, 'ReWrite': 126,
+    'Append': 127, 'Close': 128, 'Read': 129, 'ReadLn': 130, 'Write': 131, 'WriteLn': 132,
+    ',': 133, '1': 134, '2': 135, '3': 136, '4': 137, '5': 138, '6': 139, '7': 140, '8': 140,
+    '9': 141, '0': 142
 }
