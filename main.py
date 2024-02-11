12f386a98395c7478afb0a88c14bf8cc8a6e4ce4
Аня Сусак <anna.i.susak@gmail.com>
2024-02-11 21:02:06 +0300

    normal readable working code without bags with tokenizing and simple check

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import re
 from Levenshtein import distance as lev
 
 import tokenize_code_numbers
@@ -6,145 +5,133 @@ import tokenize_code_numbers
 special_words = tokenize_code_numbers.code_numbers
 
 
+def check_special_word(s):
+    for i in special_words.keys():
+        if s.startswith(i):
+            counter = 1
+            if len(i) == 1:
+                s = s[1:]
+                while s.startswith(i):
+                    counter += 1
+                    s = s[1:]
+            return [special_words[i], counter, i * counter]
+    return False
+
+
+def check_spaces(s):
+    if s[0] != ' ':
+        return False
+    counter = 0
+    i = 0
+    while s[i] == ' ':
+        counter += 1
+        i += 1
+    return [tokenize_code_numbers.SPACE_NUM, counter, ' ' * counter]
+
+
+def check_comment(s):
+    if s[0] != '{':
+        return False
+    i = 0
+    while s[i] != '}':
+        i += 1
+    return [tokenize_code_numbers.COMMENT_NUM, 1, s[:i + 1]]
+
+
+def check_string_value(s):
+    if s[0] != '\'':
+        return False
+    i = 0
+    while s[i] != '\'':
+        i += 1
+    return [tokenize_code_numbers.STRING_VALUE_NUM, 1, s[:i + 1]]
+
+
+def check_number(s):
+    if s[0] not in '1234567890':
+        return False
+    cur_var = ''
+    l = 0
+    # print(s)
+    while l < len(s) and s[l] in '1234567890':
+        # print(l, s[l], s[l] in '1234567890')
+        cur_var += s[l]
+        l += 1
+    # print('tok',[tokenize_code_numbers.NUMBER_NUM, len(cur_var), cur_var])
+    return [tokenize_code_numbers.NUMBER_NUM, len(cur_var), cur_var]
+
+
+def get_value(s):
+    val = ''
+    i = 0
+    while i < len(s) and s[i] != ' ' and s[i] not in special_words.keys():
+        val += s[i]
+        i += 1
+    return [tokenize_code_numbers.VARIABLE_NUM, 1, s[:i]]
+
+
+def check_new_string(s):
+    if s[0] != '\n':
+        return False
+    i = 0
+    counter = 0
+    while i < len(s) and s[i] == '\n':
+        counter += 1
+        i += 1
+    return [tokenize_code_numbers.NEXT_LINE_NUM, counter, '\n' * counter]
+
+
 def get_tokens(code):
     tokens = []
-    cur_w = ''
-    prev_w = ''
-    counter = 0
-    flag = False
-    '''special = {'+', '-', '*', '/', ':', '=', '<', '>', '.', '(', ')', '[', ']',
-               '{', '}', ':', ';', '\'', "\"", '$', '@', '#', '&', '^', '_', '~',
-               '%', 'and', 'end', 'nil', 'set', 'array', 'file', 'not', 'then', 'begin', 'for',
-               'of', 'to', 'case', 'function', 'or', 'type', 'const', 'goto', 'packed', 'until',
-               'div', 'if', 'procedure', 'var', 'do', 'in', 'program', 'while',
-               'downto', 'label', 'record', 'with', 'else', 'mod', 'repeat', 'writeln',
-               'maxlongint', ';', 'maxint', 'read', 'integer', 'real', 'boolean', 'char', 'string',
-               'Byte', 'ShortInt', 'Word', 'Integer', 'LongInt', 'Real', 'Single', 'Double', 'Extended',
-               'Comp', 'Boolean', 'Char', 'Array', 'String', 'Record', 'Set', 'Text', 'File',
-               '>=', '<=', 'or', 'not', 'xor', 'true', 'false', 'sin', 'cos', 'arctan', 'abs',
-               'ln', 'exp', 'sqr', 'sqrt', 'pi', 'round', 'trunc', 'frac', 'random', 'odd', 'ord',
-               'chr', 'pred', 'succ', 'readln', 'write', 'Assign', 'Reset', 'ReWrite', 'Append',
-               'Close', 'Read', 'ReadLn', 'Write', 'WriteLn', ',', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0'}'''
-    i = 0
-    while i < len(code):
-        if code[i] == '{':
-            cur = '{'
-            while i < len(code) and code[i] != '}':
-                i += 1
-                cur += code[i]
-            i += 1
-            print('comment', cur)
-            tokens.append([tokenize_code_numbers.COMMENT_NUM, 1, cur])
-        elif code[i] == "'":
-            cur = "'"
-            while i < len(code) and code[i] != "'":
-                i += 1
-                cur += code[i]
-            i += 1
-            print('string value', cur)
-            tokens.append([tokenize_code_numbers.STRING_VALUE_NUM, 1, cur])
-        elif cur_w == 'in' and i < len(code) - 1 and code[i] == 't':
-            tokens.append([special_words['integer'], 1, 'integer'])
-            i += 5
-        elif cur_w == 'write' and i < len(code) - 1 and code[i] == 'l':
-            tokens.append([special_words['writeln'], 1, 'writeln'])
-            i += 2
-        elif cur_w == 'Read' and i < len(code) - 1 and code[i] == 'L':
-            tokens.append([special_words['ReadLn'], 1, 'ReadLn'])
-            i += 2
-        elif cur_w == 'Write' and i < len(code) - 1 and code[i] == 'L':
-            tokens.append([special_words['WriteLn'], 1, 'WriteLn'])
-            i += 2
-        elif code[i] == ' ' or (code[i] in special_words.keys() and code[i] not in '0123456789'):
-            elem = code[i]
-            if cur_w != '':
-                if cur_w in special_words.keys():
-                    print('append', cur_w)
-                    tokens.append([special_words[cur_w], 1, cur_w])
-                else:
-                    print('variable', cur_w)
-                    tokens.append([tokenize_code_numbers.VARIABLE_NUM, 1, cur_w])
-                cur_w = ''
-            counter = 1
-            while (i < len(code) - 1 and (
-                    code[i + 1] == elem or (code[i + 1] in '0123456789' and elem in '0123456789'))):
-                counter += 1
-                i += 1
-            if elem != ' ':
-                tokens.append([special_words[elem], elem, counter])
-            else:
-                tokens.append([tokenize_code_numbers.SPACE_NUM, elem, counter])
-            print('elem', elem, tokens[-1])
-            cur_w = ''
-        elif code[i] in special_words.keys() and code[i] in '0123456789':
-            cur = code[i]
-            i += 1
-            while i < len(code) and code[i] in '0123456789':
-                cur += code[i]
-                i += 1
-            tokens.append([tokenize_code_numbers.NUMBER_NUM, 1, cur])
-            '''elif cur_w in special:
-            while (i < len(code) and code[i] in '0123456789'):
-                counter += 1
-                i+=1
-            tokens.append([cur_w, 1])
-            cur_w = '''
-        elif cur_w in special_words.keys():
-            tokens.append([special_words[cur_w], 1, cur_w])
-        else:
-            cur_w += code[i]
-        i += 1
-    k = 1
-    print(tokens)
-    while k < len(tokens):
-        if tokens[k][0] == tokens[k - 1][0]:
-            print('remove', k, tokens[k])
-            tokens.pop(k)
-            k -= 1
-            # print(tokens)
-        elif tokens[k][0] == tokenize_code_numbers.NUMBER_NUM and tokens[k - 1][
-                2] == tokenize_code_numbers.NUMBER_NUM:
-            print('remove', k, tokens[k])
-            tokens.pop(k)
-            k -= 1
-        elif tokens[k][2] == '=' and tokens[k - 1][2] == ':':
-            tokens[k - 1] = [special_words[':=', 1, ':=']]
-            print('remove', k, tokens[k])
-            tokens.pop(k)
-            k -= 1
-        elif tokens[k][2] == '}':
-            print('remove', k, tokens[k])
-            tokens.pop(k)
-            k -= 1
-        k += 1
+    while code != '':
+        cur = check_comment(code)
+        if cur == False:
+            cur = check_spaces(code)
+            if cur == False:
+                cur = check_new_string(code)
+                if cur == False:
+                    cur = check_special_word(code)
+                    if cur == False:
+                        cur = check_string_value(code)
+                        if cur == False:
+                            cur = check_number(code)
+                            if cur == False:
+                                cur = get_value(code)
+        tokens.append(cur)
+        code = code[len(tokens[-1][2]):]
     return tokens
 
 
 with open('test_code1.txt', 'r', encoding="utf-8") as file:
     code1 = file.read()
-# code1 = format_code((code1))
-with open('test_code3.txt', 'r', encoding="utf-8") as file:
+with open('test_code2.txt', 'r', encoding="utf-8") as file:
     code2 = file.read()
-# code2 = format_code(code2)
-# print(code1)
 t1 = get_tokens(code1)
 t2 = get_tokens(code2)
-print(t1)
-print(t2)
+print('tokens')
+k = 0
+while k < min(len(t1), len(t2)):
+    print(t2[k])
+    k += 1
+while k < max(len(t1), len(t2)):
+    if len(t1) > len(t2):
+        print(t1[k])
+    else:
+        print(t2[k])
+    k += 1
 i = 0
 j = 0
 flag = False
 while i < len(t1) and j < len(t2):
-    while i < len(t1) and t1[i][0] == tokenize_code_numbers.COMMENT_NUM or t1[i][
+    while i < len(t1) and (t1[i][0] == tokenize_code_numbers.COMMENT_NUM or t1[i][
         0] == tokenize_code_numbers.SPACE_NUM or t1[i][
-        0] == tokenize_code_numbers.STRING_VALUE_NUM:
+        0] == tokenize_code_numbers.STRING_VALUE_NUM or t1[i][0] == tokenize_code_numbers.NEXT_LINE_NUM):
         i += 1
-    while j < len(t2) and t2[j][0] == tokenize_code_numbers.COMMENT_NUM or t2[j][
+    while j < len(t2) and (t2[j][0] == tokenize_code_numbers.COMMENT_NUM or t2[j][
         0] == tokenize_code_numbers.SPACE_NUM or t2[j][
-        0] == tokenize_code_numbers.STRING_VALUE_NUM:
+        0] == tokenize_code_numbers.STRING_VALUE_NUM or t2[j][0] == tokenize_code_numbers.NEXT_LINE_NUM):
         j += 1
-    if t1[i][0] != t2[j][0]:
-        print(i, j, t1[i][0], t2[j][0])
+    if i < len(t1) and j < len(t2) and (t1[i][0] != t2[j][0]):
         flag = True
         break
     i += 1
--- tokenize_code_numbers.py
+++ tokenize_code_numbers.py
@@ -3,8 +3,9 @@ STRING_VALUE_NUM = 144
 VARIABLE_NUM = 145
 NUMBER_NUM = 146
 SPACE_NUM = 147
+NEXT_LINE_NUM = 149
 code_numbers = {
-    '+': 1, '-': 2, '*': 3, '/': 4, ':': 5, '=': 6,
+    '+': 1, '-': 2, '*': 3, '/': 4, ':=':148, ':': 5, '=': 6,
     '<': 7, '>': 8, '.': 9, '(': 10, ')': 11, '[': 12,
     ']': 13, '{': 14, '}': 15, ':': 16, ';': 17, '\'': 18,
     "\"": 19, '$': 20, '@': 21, '#': 22, '&': 23, '^': 24,
@@ -13,9 +14,9 @@ code_numbers = {
     'begin': 40, 'for': 41, 'of': 42, 'to': 43, 'case': 44,
     'function': 45, 'or': 47, 'type': 48, 'const': 49, 'goto': 50,
     'packed': 51, 'until': 52, 'div': 53, 'if': 54, 'procedure': 55,
-    'var': 56, 'do': 57, 'in': 58, 'program': 59, 'while': 60, 'downto': 61,
+    'var': 56, 'do': 57,'integer': 73, 'in': 58, 'program': 59, 'while': 60, 'downto': 61,
     'label': 62, 'record': 63, 'with': 64, 'else': 65, 'mod': 66, 'repeat': 67,
-    'writeln': 68, 'maxlongint': 69, ';': 70, 'maxint': 71, 'read': 72, 'integer': 73,
+    'writeln': 68, 'maxlongint': 69, ';': 70, 'maxint': 71, 'read': 72,
     'real': 74, 'boolean': 75, 'char': 76, 'string': 77, 'Byte': 78, 'ShortInt': 79,
     'Word': 80, 'Comp': 81, 'Integer': 82, 'LongInt': 83, 'Real': 84, 'Single': 85,
     'Double': 87, 'Extended': 88, 'Boolean': 89, 'Char': 90, 'Array': 91, 'String': 92,
@@ -24,7 +25,6 @@ code_numbers = {
     'abs': 107, 'ln': 108, 'exp': 109, 'sqr': 110, 'sqrt': 111, 'pi': 112, 'round': 113,
     'trunc': 114, 'frac': 115, 'random': 116, 'odd': 117, 'ord': 118, 'chr': 119, 'pred': 120,
     'succ': 121, 'readln': 122, 'write': 123, 'Assign': 124, 'Reset': 125, 'ReWrite': 126,
-    'Append': 127, 'Close': 128, 'Read': 129, 'ReadLn': 130, 'Write': 131, 'WriteLn': 132,
-    ',': 133, '1': 134, '2': 135, '3': 136, '4': 137, '5': 138, '6': 139, '7': 140, '8': 140,
-    '9': 141, '0': 142
+    'Append': 127, 'Close': 128, 'ReadLn': 130,'Read': 129,'WriteLn': 132, 'Write': 131,
+    ',': 133
 }
